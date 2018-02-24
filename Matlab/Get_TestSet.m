0f9ece29ed93b214bb1c6a542eaacdeba37caa91
Jean-Michel Amath Sarr <jmas902010@gmail.com>
2018-02-24 18:28:15 +0000

    Update Get_TestSet.m

--- Matlab/Get_TestSet.m
+++ Matlab/Get_TestSet.m
@@ -4,8 +4,8 @@
 cd('/Volumes/AWA_bck/2011/ConvertedHac/Cruise_2011/MatData')
 data = matfile('Echogram.mat');
 
-no_select = 100000;     % Numbers of examples to subset
-seed = 1;    % the seed plays a role to select a random start and then subset no_select
+no_select = 30000;     % Numbers of examples to subset
+seed = 2;    % the seed plays a role to select a random start and then subset no_select
                 % examples from that.
 m = length(data.Time);      % Total number of examples in the dataset
 RandomIndex = RandSelectOne(m,no_select,seed);  
@@ -15,7 +15,7 @@ Time = data.Time(1,RandomIndex);
 Echogram = data.Echogram18(:,RandomIndex);
 Depth = data.Depth18;
 
-cd('/Volumes/AWA_bck/2011/ConvertedHac/Cruise_2011/Treatment20170724_140100/DataFilterResults')
+cd('/Volumes/AWA_bck/2011/ConvertedHac/Cruise_2011/Treatment2017/DataFilterResults')
 data = matfile('Filtering.mat');
 CleanBottom = data.CleanBottom(1,RandomIndex);
 
